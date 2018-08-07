e0c01780f026fe3aa8f3bd0a655447711c16caf0
QQQOOO <741229443@qq.com>
2018-08-07 16:09:45 +0800

    update log

--- Qlink/Main/Log/DebugLogViewController.m
+++ Qlink/Main/Log/DebugLogViewController.m
@@ -47,7 +47,6 @@
 
 - (IBAction)clearAction:(id)sender {
 //    [[DDLog sharedInstance] removeAllLoggers]; // 移除log
-    
     DDFileLogger *fileLogger = [[DDFileLogger alloc] init];
     NSString *logDirectory = [fileLogger.logFileManager logsDirectory];
     NSArray <NSString *>*logsNameArray = [fileLogger.logFileManager sortedLogFileNames];
@@ -69,20 +68,9 @@
     });
 }
 
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
