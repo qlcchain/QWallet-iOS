c7704f0fdd3300aa0cd579bd3c6e9b52c658b874
kuangzihui <kuangzihui@163.com>
2020-06-11 10:20:43 +0800

    update .gitignore

--- Carthage/Checkouts/SwiftTheme/Pods/Target Support Files/Pods-PlistDemo/Pods-PlistDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PlistDemo : NSObject
-@end
-@implementation PodsDummy_Pods_PlistDemo
-@end
--- Carthage/Checkouts/TMCache/build_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-xcodebuild ONLY_ACTIVE_ARCH=NO -project tests/TMCache.xcodeproj -scheme TMCacheTests -sdk iphonesimulator TEST_AFTER_BUILD=YES clean build
